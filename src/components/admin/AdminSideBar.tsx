dac2e16d6fa6d9db54a3f3f516e9ca3b7f826bf6
BDU120314 <tsecha6@gmail.com>
2024-02-26 22:02:03 +0300

    dashboard resposivrness is done

--- src/components/admin/AdminSideBar.tsx
+++ src/components/admin/AdminSideBar.tsx
@@ -23,107 +23,119 @@ const AdminSideBar = () => {
   };
 
   return (
-    <aside
-      className={`fixed left-0 top-0 w-64 h-full bg-[#f8f4f3] p-4 z-50 transform ${
-        isClicked ? "-translate-x-full" : "translate-x-0"
-      } `}
-    >
-      <Link
-        to="/admin"
-        className="items-center pb-4 border-b border-b-gray-800"
+    <>
+      <aside
+        className={`fixed left-0 top-0 w-64 h-full bg-[#f8f4f3] p-4 z-50 transform ${
+          isClicked ? "-translate-x-full" : "translate-x-0"
+        } `}
       >
-        <h2 className="font-bold text-2xl">
-          Equb
-          <span className="bg-[#008B8B] text-white px-2 rounded-md">Admin</span>
-        </h2>
-      </Link>
-      <ul className="mt-4">
-        <span className="text-gray-400 font-bold">ADMIN</span>
-        <li onClick={handleCloseSideBar} className="mb-1 group">
-          <Link
-            to="/admin"
-            className="flex font-semibold items-center py-2 px-4 text-gray-900 hover:bg-gray-950 hover:text-gray-100 rounded-md group-[.active]:bg-gray-800 group-[.active]:text-white group-[.selected]:bg-gray-950 group-[.selected]:text-gray-100"
-          >
-            <RiHome2Line className="mr-3 text-xl" />
-            <span className="text-sm">Dashboard</span>
-          </Link>
-        </li>
-        <li onClick={handleCloseSideBar} className="mb-1 group">
-          <Link
-            to=""
-            className="flex font-semibold items-center py-2 px-4 text-gray-900 hover:bg-gray-950 hover:text-gray-100 rounded-md group-[.active]:bg-gray-800 group-[.active]:text-white group-[.selected]:bg-gray-950 group-[.selected]:text-gray-100 s"
-          >
-            <RiUserLine className="mr-3 text-xl" />
-            <span className="text-sm">Manage Users</span>
-            <i className="ri-arrow-right-s-line ml-auto group-[.selected]:rotate-90"></i>
-          </Link>
-        </li>
-        <li onClick={handleCloseSideBar} className="mb-1 group">
-          <Link
-            to=""
-            className="flex font-semibold items-center py-2 px-4 text-gray-900 hover:bg-gray-950 hover:text-gray-100 rounded-md group-[.active]:bg-gray-800 group-[.active]:text-white group-[.selected]:bg-gray-950 group-[.selected]:text-gray-100"
-          >
-            <RiExchangeDollarFill className="mr-3 text-xl" />
-            <span className="text-sm">Transactions</span>
-          </Link>
-        </li>
+        <Link
+          to="/admin"
+          className="items-center pb-4 border-b border-b-gray-800"
+        >
+          <h2 className="font-bold text-2xl">
+            Equb
+            <span className="bg-[#008B8B] text-white px-2 rounded-md">
+              Admin
+            </span>
+          </h2>
+        </Link>
+        <ul className="mt-4">
+          <span className="text-gray-400 font-bold">ADMIN</span>
+          <li onClick={handleCloseSideBar} className="mb-1 group">
+            <Link
+              to="/admin"
+              className="flex font-semibold items-center py-2 px-4 text-gray-900 hover:bg-gray-950 hover:text-gray-100 rounded-md group-[.active]:bg-gray-800 group-[.active]:text-white group-[.selected]:bg-gray-950 group-[.selected]:text-gray-100"
+            >
+              <RiHome2Line className="mr-3 text-xl" />
+              <span className="text-sm">Dashboard</span>
+            </Link>
+          </li>
+          <li onClick={handleCloseSideBar} className="mb-1 group">
+            <Link
+              to=""
+              className="flex font-semibold items-center py-2 px-4 text-gray-900 hover:bg-gray-950 hover:text-gray-100 rounded-md group-[.active]:bg-gray-800 group-[.active]:text-white group-[.selected]:bg-gray-950 group-[.selected]:text-gray-100 s"
+            >
+              <RiUserLine className="mr-3 text-xl" />
+              <span className="text-sm">Manage Users</span>
+              <i className="ri-arrow-right-s-line ml-auto group-[.selected]:rotate-90"></i>
+            </Link>
+          </li>
+          <li onClick={handleCloseSideBar} className="mb-1 group">
+            <Link
+              to=""
+              className="flex font-semibold items-center py-2 px-4 text-gray-900 hover:bg-gray-950 hover:text-gray-100 rounded-md group-[.active]:bg-gray-800 group-[.active]:text-white group-[.selected]:bg-gray-950 group-[.selected]:text-gray-100"
+            >
+              <RiExchangeDollarFill className="mr-3 text-xl" />
+              <span className="text-sm">Transactions</span>
+            </Link>
+          </li>
 
-        <span className="text-gray-400 font-bold">Equb Groups</span>
-        <li onClick={handleCloseSideBar} className="mb-1 group">
-          <Link
-            to=""
-            className="flex font-semibold items-center py-2 px-4 text-gray-900 hover:bg-gray-950 hover:text-gray-100 rounded-md group-[.active]:bg-gray-800 group-[.active]:text-white group-[.selected]:bg-gray-950 group-[.selected]:text-gray-100 sidebar-dropdown-toggle"
-          >
-            <RiGroupLine className="mr-3 text-xl" />
-            <span className="text-sm">Manage Groups</span>
-            <i className="ri-arrow-right-s-line ml-auto group-[.selected]:rotate-90"></i>
-          </Link>
-          <ul className="pl-7 mt-2 hidden group-[.selected]:block">
-            <li className="mb-4">
-              <Link
-                to=""
-                className="text-gray-900 text-sm flex items-center hover:text-[#f84525] before:contents-[''] before:w-1 before:h-1 before:rounded-full before:bg-gray-300 before:mr-3"
-              >
-                All
-              </Link>
-            </li>
-            <li className="mb-4">
-              <Link
-                to=""
-                className="text-gray-900 text-sm flex items-center hover:text-[#f84525] before:contents-[''] before:w-1 before:h-1 before:rounded-full before:bg-gray-300 before:mr-3"
-              >
-                Categories
-              </Link>
-            </li>
-          </ul>
-        </li>
-        <span className="text-gray-400 font-bold">PERSONAL</span>
-        <li onClick={handleCloseSideBar} className="mb-1 group">
-          <Link
-            to=""
-            className="flex font-semibold items-center py-2 px-4 text-gray-900 hover:bg-gray-950 hover:text-gray-100 rounded-md group-[.active]:bg-gray-800 group-[.active]:text-white group-[.selected]:bg-gray-950 group-[.selected]:text-gray-100"
-          >
-            <RiNotification4Line className="mr-3 text-xl" />
-            <span className="text-sm">Notifications</span>
-            <span className=" md:block px-2 py-0.5 ml-auto text-xs font-medium tracking-wide text-red-600 bg-red-200 rounded-full">
-              5
-            </span>
-          </Link>
-        </li>
-        <li onClick={handleCloseSideBar} className="mb-1 group">
-          <Link
-            to="/admin/profile"
-            className="flex font-semibold items-center py-2 px-4 text-gray-900 hover:bg-gray-950 hover:text-gray-100 rounded-md group-[.active]:bg-gray-800 group-[.active]:text-white group-[.selected]:bg-gray-950 group-[.selected]:text-gray-100"
-          >
-            <RiMessage2Line className="mr-3 text-xl" />
-            <span className="text-sm">Messages</span>
-            <span className=" md:block px-2 py-0.5 ml-auto text-xs font-medium tracking-wide text-green-600 bg-green-200 rounded-full">
-              2 New
-            </span>
-          </Link>
-        </li>
-      </ul>
-    </aside>
+          <span className="text-gray-400 font-bold">Equb Groups</span>
+          <li onClick={handleCloseSideBar} className="mb-1 group">
+            <Link
+              to=""
+              className="flex font-semibold items-center py-2 px-4 text-gray-900 hover:bg-gray-950 hover:text-gray-100 rounded-md group-[.active]:bg-gray-800 group-[.active]:text-white group-[.selected]:bg-gray-950 group-[.selected]:text-gray-100 sidebar-dropdown-toggle"
+            >
+              <RiGroupLine className="mr-3 text-xl" />
+              <span className="text-sm">Manage Groups</span>
+              <i className="ri-arrow-right-s-line ml-auto group-[.selected]:rotate-90"></i>
+            </Link>
+            <ul className="pl-7 mt-2 hidden group-[.selected]:block">
+              <li className="mb-4">
+                <Link
+                  to=""
+                  className="text-gray-900 text-sm flex items-center hover:text-[#f84525] before:contents-[''] before:w-1 before:h-1 before:rounded-full before:bg-gray-300 before:mr-3"
+                >
+                  All
+                </Link>
+              </li>
+              <li className="mb-4">
+                <Link
+                  to=""
+                  className="text-gray-900 text-sm flex items-center hover:text-[#f84525] before:contents-[''] before:w-1 before:h-1 before:rounded-full before:bg-gray-300 before:mr-3"
+                >
+                  Categories
+                </Link>
+              </li>
+            </ul>
+          </li>
+          <span className="text-gray-400 font-bold">PERSONAL</span>
+          <li onClick={handleCloseSideBar} className="mb-1 group">
+            <Link
+              to=""
+              className="flex font-semibold items-center py-2 px-4 text-gray-900 hover:bg-gray-950 hover:text-gray-100 rounded-md group-[.active]:bg-gray-800 group-[.active]:text-white group-[.selected]:bg-gray-950 group-[.selected]:text-gray-100"
+            >
+              <RiNotification4Line className="mr-3 text-xl" />
+              <span className="text-sm">Notifications</span>
+              <span className=" md:block px-2 py-0.5 ml-auto text-xs font-medium tracking-wide text-red-600 bg-red-200 rounded-full">
+                5
+              </span>
+            </Link>
+          </li>
+          <li onClick={handleCloseSideBar} className="mb-1 group">
+            <Link
+              to="/admin/profile"
+              className="flex font-semibold items-center py-2 px-4 text-gray-900 hover:bg-gray-950 hover:text-gray-100 rounded-md group-[.active]:bg-gray-800 group-[.active]:text-white group-[.selected]:bg-gray-950 group-[.selected]:text-gray-100"
+            >
+              <RiMessage2Line className="mr-3 text-xl" />
+              <span className="text-sm">Messages</span>
+              <span className=" md:block px-2 py-0.5 ml-auto text-xs font-medium tracking-wide text-green-600 bg-green-200 rounded-full">
+                2 New
+              </span>
+            </Link>
+          </li>
+        </ul>
+      </aside>
+      {/* <div
+        onClick={() => {
+          dispatch(closeMenuBar());
+        }}
+        className={`fixed top-0 left-0 w-full h-full bg-black/50 z-40 md:hidden ${
+          isClicked ? "hidden" : ""
+        }  `}
+      ></div> */}
+    </>
   );
 };
 
--- src/pages/AdminDashboard.tsx
+++ src/pages/AdminDashboard.tsx
@@ -5,13 +5,12 @@ import { useSelector } from "react-redux";
 import { RootState } from "../Redux/store";
 const AdminDashboard = () => {
   const isClicked = useSelector((state: RootState) => state.user.isClicked);
-  console.log(isClicked);
   return (
     <div className="flex text-gray-800 font-inter relative">
       <AdminSideBar />
       <div
-        className={`w-full relative flex flex-col flex-1 md:ml-64 bg-gray-200 min-h-screen transition-all ${
-          isClicked ? "md:ml-0" : "md:w-[calc(100%-256px)]"
+        className={`w-full relative flex flex-col flex-1 bg-gray-200 min-h-screen transition-all ${
+          isClicked ? "md:ml-0" : "md:ml-64 md:w-[calc(100%-256px)]"
         }`}
       >
         <AdminHeader />
