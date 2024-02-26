4fd4a5e041c8f448db3e285eea65b67f3f212635
nathnael-chilot <chilotnathnael@gmail.com>
2024-02-27 01:19:09 +0300

    create a line chart and update card data

--- src/Redux/store.ts
+++ src/Redux/store.ts
@@ -1,9 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-<<<<<<< HEAD
-import UserReducer from './Features/userSlice'
-=======
 import UserReducer from "./Features/userSlice";
->>>>>>> a8265ca03a444941276f1bd0f2e9346a2dfae759
 
 export const store = configureStore({
   reducer: {
@@ -12,12 +8,6 @@ export const store = configureStore({
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
-<<<<<<< HEAD
-export type RootState = ReturnType<typeof store.getState>
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch
-=======
 export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
->>>>>>> a8265ca03a444941276f1bd0f2e9346a2dfae759
--- src/components/UI/LineChart.tsx
+++ src/components/UI/LineChart.tsx
@@ -1,18 +1,6 @@
-<<<<<<< HEAD
-import React from "react"
-
-const LineChart = () => {
-  return (
-    <div>LineChart</div>
-  )
-}
-
-export default LineChart
-=======
 import React from "react";
 const LineChart = () => {
   return <div>LineChart</div>;
 };
 
 export default LineChart;
->>>>>>> a8265ca03a444941276f1bd0f2e9346a2dfae759
