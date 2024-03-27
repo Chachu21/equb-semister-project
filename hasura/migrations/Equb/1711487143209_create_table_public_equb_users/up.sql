1cd5a55955ca4b868c682181edec2a18493dce71
nathnael-chilot <chilotnathnael@gmail.com>
2024-03-27 20:14:04 +0300

    recieve initiallization

--- hasura/migrations/Equb/1711487143209_create_table_public_equb_users/up.sql
+++
@@ -1,2 +0,0 @@
-CREATE TABLE "public"."equb_users" ("id" uuid NOT NULL DEFAULT gen_random_uuid(), "full_name" text NOT NULL, "email" text NOT NULL, "phone_no" text NOT NULL, "password" text NOT NULL, "agreedTerms" boolean NOT NULL DEFAULT false, PRIMARY KEY ("id") , UNIQUE ("id"), UNIQUE ("email"));
-CREATE EXTENSION IF NOT EXISTS pgcrypto;
